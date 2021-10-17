d440786f8843a12df89fb00566c87372f94db324
Joshua Rehak <jsrehak@gmail.com>
2021-10-17 09:21:08 -0700

    updated volume scripts

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "  "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
- amixer -c 1 get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "Muted "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
+ amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "  "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
+# amixer -c 1 get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "Muted "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
